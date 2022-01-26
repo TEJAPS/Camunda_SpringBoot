f43b8fd8df606565e32e418599996a51e8739b36
TEJAPS <tejapolisetty143@gmail.com>
2022-01-27 02:29:50 +0530

    added dummy router for tasklist admin and cockpit

--- custom-camunda-frontend/src/App.js
+++ custom-camunda-frontend/src/App.js
@@ -8,17 +8,29 @@ import Footer from "./components/Footer";
 import "./index.css";
 import DiagramMenuContentComponent from "./components/tasklist/taskDetailMenu/DiagramMenuContentComponent";
 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import TaskListIndexComponent from "./components/tasklist/TaskListIndexComponent";
+import AdminIndexComponent from "./components/admin/AdminIndexComponent";
+import CockPitIndexComponent from "./components/cockpit/CockPitIndexComponent";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div class="taskfegroup">
-        <Filter />
-        <Tasklist />
+    <BrowserRouter>
+      <div className="App">
+        {/* <Header />
+        <TaskListIndexComponent />
+        <Footer /> */}
+        {/* <Switch> */}
+        <Link to="/tasklist">Tasklist</Link>
+        <Link to="/admin">Admin</Link>
+        <Link to="/cockpit">Cockpit</Link>
+        <Header />
+        <Route exact path="/tasklist" component={TaskListIndexComponent} />
+        <Route exact path="/admin" component={AdminIndexComponent} />
+        <Route exact path="/cockpit" component={CockPitIndexComponent} />
+        <Footer />
+        {/* </Switch> */}
       </div>
-      <Footer />
-      {/* <DiagramMenuContentComponent /> */}
-    </div>
+    </BrowserRouter>
   );
 }
 
